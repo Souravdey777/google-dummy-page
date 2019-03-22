b69b8b40ee32fee3cc2c5546c4a148173854de3c
System Administrator <root@Tests-iMac.local>
2019-03-22 18:01:25 +0530

    Updates

---
+++ precache-manifest.33b5756e9668e72c406c185aac9dd14a.js
@@ -0,0 +1,74 @@
+self.__precacheManifest = [
+  {
+    "revision": "6bd5905965186c20225f0b11320dcdfe",
+    "url": "/google-dummy-page/static/media/googleNews.6bd59059.png"
+  },
+  {
+    "revision": "91ce5aaa5bbd0908b6b8",
+    "url": "/google-dummy-page/static/css/main.504d2be0.chunk.css"
+  },
+  {
+    "revision": "a664aacf683d14919140",
+    "url": "/google-dummy-page/static/js/runtime~main.20cd405c.js"
+  },
+  {
+    "revision": "815bc961380098f54654",
+    "url": "/google-dummy-page/static/js/2.4975e070.chunk.js"
+  },
+  {
+    "revision": "5e73adb0252c16df376aafd9c944a1a8",
+    "url": "/google-dummy-page/static/media/user.5e73adb0.jpg"
+  },
+  {
+    "revision": "80fa4bcab0351fdccb69c66fb55dcd00",
+    "url": "/google-dummy-page/static/media/googlelogo.80fa4bca.png"
+  },
+  {
+    "revision": "98a94401eaf7463d74c81f571c61cfaf",
+    "url": "/google-dummy-page/static/media/Google_mic.98a94401.png"
+  },
+  {
+    "revision": "dad2164f61019c8cd5e01c5a2dd25c73",
+    "url": "/google-dummy-page/static/media/favicon.dad2164f.png"
+  },
+  {
+    "revision": "fc38a6e41b3f646f47ab14deccf52c4e",
+    "url": "/google-dummy-page/static/media/maps.fc38a6e4.png"
+  },
+  {
+    "revision": "91ce5aaa5bbd0908b6b8",
+    "url": "/google-dummy-page/static/js/main.83207dcc.chunk.js"
+  },
+  {
+    "revision": "598d07e7761f5bca3a3f78e95b6db422",
+    "url": "/google-dummy-page/static/media/gmail.598d07e7.png"
+  },
+  {
+    "revision": "336a0e4fc93f0ffaa30c0e69bc118bab",
+    "url": "/google-dummy-page/static/media/contacts.336a0e4f.png"
+  },
+  {
+    "revision": "f2507f141b182b21cedc1a063fdb05b3",
+    "url": "/google-dummy-page/static/media/drive.f2507f14.png"
+  },
+  {
+    "revision": "7c6307d1f88c2e6cad0c74aaa02a33fa",
+    "url": "/google-dummy-page/static/media/calendar.7c6307d1.png"
+  },
+  {
+    "revision": "5715f4c7f3917925dee513efcbe05a71",
+    "url": "/google-dummy-page/static/media/googleplus.5715f4c7.png"
+  },
+  {
+    "revision": "1cd72012aed6c6c622eaa04bdb4eec73",
+    "url": "/google-dummy-page/static/media/GoogleTranslate.1cd72012.png"
+  },
+  {
+    "revision": "163fe87137ca3e9e509add5af64d07a3",
+    "url": "/google-dummy-page/static/media/photos.163fe871.ico"
+  },
+  {
+    "revision": "f59a01d698d694a936a9e0f2c005c9fa",
+    "url": "/google-dummy-page/index.html"
+  }
+];
